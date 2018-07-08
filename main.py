760c19dc86a6f13f98f832c540d82445eedd90b3
Daghlny <Daghlny@gmail.com>
2018-07-08 14:13:48 +0800

    add new codes on query orders

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class OrdersService(object):
         worker_key = keys.worker_k_prefix + str(userid)
         master_key = keys.master_k_prefix + str(userid)
 
-        other_orders_keys = self.rds.sdiff(keys.pending_orders_k, worker_key, master_key)
+        other_orders_keys  = self.rds.sdiff(keys.pending_orders_k, worker_key, master_key)
         worker_orders_keys = self.rds.smembers(worker_key)
         master_orders_keys = self.rds.smembers(master_key)
 
@@ -39,28 +39,39 @@ class OrdersHandler(tornado.web.RequestHandler):
     service = OrdersService()
     def get(self):
         userid = self.get_argument("user_id")
-        my_worker_orders, my_master_orders, my_other_orders = self.service.getOrders(userid)
+        worker_orders_array, master_orders_array, other_orders_array = self.service.getOrders(userid)
 
-        worker_orders = list()
-        master_orders = list()
-        other_orders  = list()
+        worker_orders_str = "["
+        master_orders_str = "["
+        other_orders_str  = "["
 
-        for order_str in my_worker_orders:
-            order_obj = json.loads(order_str)
-            worker_orders.append(order_obj)
-        for order_str in my_master_orders:
-            order_obj = json.loads(order_str)
-            master_orders.append(order_obj)
-        for order_str in other_orders:
-            order_obj = json.loads(order_str)
-            other_orders.append(order_obj)
+        for i in range(0, len(worker_orders_array)):
+            if i == 0:
+                worker_orders_str += worker_orders_array[i]
+            else:
+                worker_orders_str += ","+worker_orders_array[i]
+
+        for i in range(0, len(master_orders_array)):
+            if i == 0:
+                master_orders_str += master_orders_array[i]
+            else:
+                master_orders_str += ","+master_orders_array[i]
+
+        for i in range(0, len(other_orders_array)):
+            if i == 0:
+                other_orders_str += other_orders_array[i]
+            else:
+                other_orders_str += ","+other_orders_array[i]
+
+        worker_orders_str += "]"
+        master_orders_str += "]"
+        other_orders_str  += "]"
             
-        result = {
-            "my_worker_orders": worker_orders,
-            "my_master_orders": master_orders,
-            "other_orders": other_orders
-        }
+        result = "{\"my_worker_orders\": "+worker_orders_str+", " + "\"my_master_orders\": "+master_orders_str+", " + "\"other_orders\": "+other_orders_str+"}"
+
         print(result)
+        print("------------------------------------------")
+        print(json.dumps(json.loads(result)))
         self.set_header("Content-Type", "application/json; charset=UTF-8")
         self.write(json.dumps(result))
         
