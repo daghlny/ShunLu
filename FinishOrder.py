1d040ffdf9980d97d57087795e38a827c99e90d5
Daghlny <Daghlny@gmail.com>
2018-07-08 16:15:06 +0800

    fix the FinishOrder.py

--- FinishOrder.py
+++ FinishOrder.py
@@ -13,6 +13,9 @@ class FinishOrderService(object):
         self.rds = redis.StrictRedis("localhost", 6379)
 
     def finish_order(self, orderid):
+        orderid = str(orderid)
+        print(orderid)
+        # if the order is not in "doing" or "worker_confirmed" sets, there must exist an error
         if self.rds.sismember("doing", orderid):
             self.rds.srem("doing", orderid)
         elif self.rds.sismember("worker_confirmed", orderid):
@@ -20,10 +23,13 @@ class FinishOrderService(object):
         else:
             return -1
         self.rds.sadd("finished", orderid)
-        json_str = self.rds.get(orderid)
+        json_str = self.rds.get(orderid).decode(charset)
         json_obj = json.loads(json_str)
         worker_id = json_obj["worker_id"]
         master_id = json_obj["master_id"]
+        # set the status of order to "finished"
+        json_obj["status"] = 6 
+        # move the orderid from worker and master's "not finished" set to "finished" set
         self.rds.srem("worker"+str(worker_id), orderid)
         self.rds.srem("master"+str(master_id), orderid)
         self.rds.sadd("finished"+str(worker_id), orderid)
@@ -31,10 +37,13 @@ class FinishOrderService(object):
         return 0
 
 class FinishOrderHandler(tornado.web.RequestHandler):
-    service = OrdersService()
+    service = FinishOrderService()
     def get(self):
-        userid = self.get_argument("order_id")
+        orderid = self.get_argument("order_id")
         ret = self.service.finish_order(orderid)
-        self.set_header("Content-Type", "application/text; charset=UTF-8")
-        self.write(str(ret))
+        self.set_header("Content-Type", "application/json; charset=UTF-8")
+        result = {"result": str(ret)}
+        self.write(json.dumps(result))
+
+
         
